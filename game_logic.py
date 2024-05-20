43df1faec04577514a3c8bfb1702eb4b9f6f972c
Anvilofsand <121681433+Anvilofsand@users.noreply.github.com>
2024-05-20 16:05:28 +0000

    setting up the AI

--- game_logic.py
+++ game_logic.py
@@ -1,9 +1,70 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+import pathlib
+import textwrap
+
 import google.generativeai as genai
 
-genai.configure(api_key='AIzaSyAqsSXE1FJo0XvqDE8pMrVyi6r1K__ruY4')
-model = genai.GenerativeModel('gemini-pro')
-context= " "
+from IPython.display import display
+from IPython.display import Markdown
+
+
+
+
+import google.generativeai as genai
+
+genai.configure( api_key = input("key: "))
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_NONE",
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_NONE",
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_NONE",
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_NONE",
+  },
+]
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro-latest",
+  safety_settings=safety_settings,
+  generation_config=generation_config,
+)
+history=" "
 while True:
-    response = model.generate_content(context + input(": "))
-    print(response.text)
-    context = context + response.text + " "
+    chat_session = model.start_chat(
+
+    )
+    user_input = input ("message:")
+    message = history + "| end of context. User message:  " + user_input
+    response = chat_session.send_message(message)
+    history = message + "                 " + str(response)
+    def to_markdown(text):
+        text = text.replace('•', '  *')
+        return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+        print(text)
+        print(chat_session.history)
