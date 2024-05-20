1adbe3e6dbb38ea3ce1cfb1eb0e37f12ee54d275
Anvilofsand <121681433+Anvilofsand@users.noreply.github.com>
2024-05-20 13:50:12 +0000

    booty cheecks

--- game_logic.py
+++ game_logic.py
@@ -1,2 +1,9 @@
-print("hello")
-def hi ():
+import google.generativeai as genai
+
+genai.configure(api_key='AIzaSyAqsSXE1FJo0XvqDE8pMrVyi6r1K__ruY4')
+model = genai.GenerativeModel('gemini-pro')
+context= " "
+while True:
+    response = model.generate_content(context + input(": "))
+    print(response.text)
+    context = context + response.text + " "
