68848db04374aa094136565d8181c14730cba13b
Anvilofsand <121681433+Anvilofsand@users.noreply.github.com>
2024-05-30 14:00:26 +0000

    still not working error 429 resource limit exhausted

--- game_logic.py
+++ game_logic.py
@@ -7,6 +7,7 @@ See the getting started guide for more information:
 https://ai.google.dev/gemini-api/docs/get-started/python
 """
 import pathlib
+
 import textwrap
 
 import google.generativeai as genai
@@ -15,15 +16,16 @@ from IPython.display import display
 from IPython.display import Markdown
 
 
-
+key = input("key: ")
+history=" "
 
 import google.generativeai as genai
 
-genai.configure( api_key = input("key: "))
+genai.configure( api_key = key)
 
-# Create the model
+    # Create the model
 # See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
-generation_config = {
+generation_config = { 
   "temperature": 1,
   "top_p": 0.95,
   "top_k": 64,
@@ -50,17 +52,17 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-pro-latest",
-  safety_settings=safety_settings,
-  generation_config=generation_config,
-)
-history=" "
+model_name="gemini-1.5-pro-latest",
+safety_settings=safety_settings,
+  
+generation_config=generation_config,)
+chat_session = model.start_chat()
 while True:
-    chat_session = model.start_chat()
-    user_input = input ("message:")
-    message = history + "| end of context. User message:  " + user_input
-    response = chat_session.send_message(user_input)
-    message = history + "\n" + user_input
-    history = message + "\n" + str(response.text)
-    print(response.text)
-    #print(chat_session.history)
+  user_input = input ("message:")
+  message = history + "" + user_input
+  response = chat_session.send_message(user_input)
+  message = history + "\n" + user_input
+  history = message + "\n" + str(response.text)
+  print(response.text) 
+#print(chat_session.history
+
