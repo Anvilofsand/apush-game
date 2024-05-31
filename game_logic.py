15b376cb44b5eacedcc44ecf29b5bfa014862119
Anvilofsand <121681433+Anvilofsand@users.noreply.github.com>
2024-05-31 17:08:58 +0000

    poaegrnjodsg;kljng;kojegwr[oegr

--- game_logic.py
+++ game_logic.py
@@ -1,11 +1,14 @@
 import google.generativeai as genai
-
-genai.configure(api_key='AIzaSyAqsSXE1FJo0XvqDE8pMrVyi6r1K__ruY4')
-model = genai.GenerativeModel('gemini-pro')
+import random
 context= " "
 current_file = ''
+file_number=1
 while True:
-    response = model.generate_content(current_file + "\n" + context + input(": "))
-    print(response.text)
-    context = context + "\n" + response.text + "\n"
-    file_number +=1
+    key = random.getrandbits
+    print (key)
+    #genai.configure(api_key=key)
+    #model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    #response = model.generate_content(current_file + "\n" + context + input(": "))
+    #print(response.text)
+    #context = context + "\n" + response.text + "\n"
+    #file_number +=1
