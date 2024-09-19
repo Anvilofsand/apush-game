93520b006b31e64e3177317963259a63df1a946e
Anvilofsand <121681433+Anvilofsand@users.noreply.github.com>
2024-09-19 13:36:15 +0000

    things

--- game_logic.py
+++ game_logic.py
@@ -1,14 +1,8 @@
 import google.generativeai as genai
 import time
 promptnum = 0
-file_number = int(input ("""pick your adventure
-1. ride of paul revere
-                         choice: """)) -1
-file_list = ["f1.text", 0, 0, 0]
-current_file = file_list [file_number]
-current_file = open(current_file, "r")
-context = current_file.read () + "off of this/these document(s) you will make an adventure that is historically accurate. You will keep the user on task and make sure to stop us from going to far from the story. you will run it in a Dnd like style where you are the game master where you descirbe the events and consequences of my actions to me once I do them; you are not to give options but wait for me to tell you what I am doing. I am the main character in whatever historical event we are in. after we have gone though the whole story you will give me a score out of 10 for how well I did. 10 is the highest 1 is the lowest. \n \n \n"
-key = 'AIzaSyAqsSXE1FJo0XvqDE8pMrVyi6r1K__ruY4'
+context = " "
+key = 'AIzaSyAbiipn9FvuHJT6-kYPSLmZgPFWxrKdhVc'
 model = ''
 error = False
 user_input =''
